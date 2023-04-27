27a0d11d31fa2cf943f45067b309aae97829011e
Mads Nicolaj Nielsen <45572200+madsen372@users.noreply.github.com>
2023-04-27 08:22:03 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
     <div className={darkMode ? "dark" : ""}>
       <Head>
         <title>Next App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" width="device-width" />
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
